5a7864c6dd153f1b2e42ad2fd041b9a75a4018a2
Varad Choudhari <varad.choudhari@gmail.com>
2018-04-15 19:52:19 -0400

    Implemented Results

--- assets/js/results.jsx
+++ assets/js/results.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {InputGroup, Button, Input} from 'reactstrap'
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card'
 
 export default class Results extends React.Component {
   render() {
     return(<div className="container-fluid">
-    <div className="results-card">
-      <div></div>
-    </div>
+    Results
+    <Card>
+      <CardHeader
+        title="Quilt"
+        subtitle="Java Developer"
+        actAsExpander={true}
+        showExpandableButton={true}
+        avatar="http://github-jobs.s3.amazonaws.com/55aa15ba-3381-11e8-8aa5-cd3357a09495.png"
+        >
+      </CardHeader>
+      <CardText expandable={true}>
+        Some descripttion of this job
+      </CardText>
+    </Card>
   </div>)
 }
 }
