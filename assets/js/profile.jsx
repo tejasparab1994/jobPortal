309e312bb52c1496a707f1951836c2dec2d139f3
Tejas Parab <tejasparab890@gmail.com>
2018-04-16 14:50:52 -0400

    code shifted to resume parser

--- assets/js/profile.jsx
+++ assets/js/profile.jsx
@@ -3,57 +3,15 @@ import ReactDOM from 'react-dom';
 import Job from './job';
 import { connect } from 'react-redux';
 import RaisedButton from 'material-ui/RaisedButton';
-
+import ResumeParser from './resumeParser';
 
 export default class Profile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      file: null
-    }
-    this.onFormSubmit = this.onFormSubmit.bind(this)
-    this.onChange = this.onChange.bind(this)
-    this.fileUpload = this.fileUpload.bind(this)
-  }
-
-  onFormSubmit(e){
-   e.preventDefault() // Stop form submit
-   this.fileUpload(this.state.file).then((response)=>{
-     console.log(response.data);
-   })
- }
-
- onChange(e) {
-   console.log("hello");
-   this.setState({file:e.target.files[0]})
-   console.log(this.state);
- }
-
-  fileUpload(file){
-
-    const url = 'http://example.com/file-upload';
-    const formData = new FormData();
-    formData.append('file',file)
-    const config = {
-        headers: {
-            'content-type': 'multipart/form-data'
-        }
-    }
-    console.log(this.state);
-    return  
-  }
 
   render() {
     return (
       <div className="container-fluid">
-        <div className="resume-upload">
-          <b>Upload your resume here: </b>
-          <form onSubmit={this.onFormSubmit}>
-            <input type="file" onChange={this.onChange}/>
-            <RaisedButton type="submit" label="Submit" className="button-submit" primary={true} />
-          </form>
-          </div>
-        </div>
+        <ResumeParser />
+      </div>
     );
   }
 }
