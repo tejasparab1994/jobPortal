be7fd4692fc56cecd124f27cbeba51066e1794ef
MSP95 <patilmanish1910@gmail.com>
2018-04-16 16:06:25 -0400

    "ResumeParser from react"

--- assets/js/resumeParser.jsx
+++ assets/js/resumeParser.jsx
@@ -1,13 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Job from './job';
-import { connect } from 'react-redux';
-import RaisedButton from 'material-ui/RaisedButton';
+
+import React from 'react'
+// import axios, { post } from 'axios';
 
 export default class ResumeParser extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state ={
+      file:null
+    }
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.fileUpload = this.fileUpload.bind(this)
+  }
+  onFormSubmit(e){
+    e.preventDefault() // Stop form submit
+    console.log(this.state.file);
+    let formData = new FormData();
+    let reader = new FileReader()
+    let file = this.state.file.name;
+    reader.readAsDataURL(this.state.file)
+    reader.addEventListener("load", ()=> {
+      console.log(file);
+      // console.log(reader.result.split(",", 2)[1]);
+      let payload = {binary: reader.result.split(",", 2)[1], filename: file}
+      console.log(payload);
+      this.props.channel.push("uploadfile", payload)
+  }, false);
+
+  }
+
+  onChange(e) {
+    this.setState({file:e.target.files[0]})
+
+  }
+  fileUpload(file){
+    const url = 'http://example.com/file-upload';
+    const formData = new FormData();
+    formData.append('file',file)
+    const config = {
+        headers: {
+            'content-type': 'multipart/form-data'
+        }
+    }
+    return  "post(url, formData,config)"
+  }
+
   render() {
     return (
-      <div>Hello from resumeparser</div>
-    );
+      <form onSubmit={this.onFormSubmit}>
+        <h1>File Upload</h1>
+        <input type="file" onChange={this.onChange} />
+        <button type="submit">Upload</button>
+      </form>
+   )
   }
 }
--- lib/jobPortal_web/channels/user_channel.ex
+++ lib/jobPortal_web/channels/user_channel.ex
@@ -7,9 +7,20 @@ defmodule JobPortalWeb.UserChannel do
   end
 
   def handle_in("search", payload, socket) do
-  %{"title" => title, "location" => location} = payload
-  data = JobPortal.ApiFetch.getJobs(title, location)
-  {:reply, {:ok, data}, socket}
-end
+    %{"title" => title, "location" => location} = payload
+    data = JobPortal.ApiFetch.getJobs(title, location)
+    {:reply, {:ok, data}, socket}
+  end
+
+  def handle_in("uploadfile", payload, socket) do
+    payload1 = payload
+    |> Map.new(fn {k, v} -> {String.to_atom(k), v} end)
+    payload2 = Map.put(payload1, :binary, Base.decode64!(payload1.binary))
+    File.write("assignment1.pdf", payload2.binary, [:binary])
+    resp = HTTPoison.post!("http://api2.pdfextractoronline.com:8089/tab2ex2/api?tab2exkey=608.3FEB7C1B3F277FB8&fileName=assignment1.pdf&recognitionMethod=auto&outputFormat=TXT", {:multipart, [{:file, "assignment1.pdf"}]})
+    IO.inspect resp.body
+    # IO.inspect payload2
+    {:reply, {:ok, payload}, socket}
+  end
 
 end
