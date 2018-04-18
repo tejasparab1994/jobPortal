2a218dbc50a9761ab8956acb320a83748ab484ff
abhishek ahuja <ahuja.a@husky.neu.edu>
2018-04-18 19:02:43 -0400

    github address bar issue resolved

--- assets/js/home.jsx
+++ assets/js/home.jsx
@@ -52,7 +52,11 @@ class Jobs extends React.Component {
             <Navbar channel={this.props.channel} token = {this.props.token}/>
             <div className= "containerd-fluid">
             <Route path="/" exact={true} render={() => <div><Search channel={this.props.channel} /><div className="container"><Results jobs={this.props.state.jobs} channel={this.props.channel}/></div></div>} />
+<<<<<<< Updated upstream
             <Route path="/githubToken" exact={false} render={() => <div><Search channel={this.props.channel}/><div className="container"><Results jobs={this.props.state.jobs} channel={this.props.channel}/></div></div>} />
+=======
+            <Route path="/githubToken" exact={false} render={(history) => <div>{history.history.push("/")}</div>} />
+>>>>>>> Stashed changes
             <Route path="/register" exact={false} render={(history) => <div> <UserForm history = {history}/> </div>} />
             <Route path="/tools" render={(history) => this.props.token ? (<Scorer channel={this.props.channel} />) : (<div>{history.history.push("/")}</div>) }/>
             <Route path="/profile" render={(history) => this.props.token ? ( <Profile channel={this.props.channel} />) : (<div> {history.history.push("/")} </div>)} />
