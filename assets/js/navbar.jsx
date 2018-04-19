2be873d08eb9e5fa85821dc7ca283ab9955fe007
abhishek ahuja <ahuja.a@husky.neu.edu>
2018-04-18 21:36:53 -0400

    added home button

--- assets/js/navbar.jsx
+++ assets/js/navbar.jsx
@@ -16,12 +16,20 @@ import {NavLink} from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import store from './store';
 import api from './api';
+import SvgIcon from 'material-ui/SvgIcon';
 function logout() {
   store.dispatch({
     type: 'LOGOUT'
   });
 
 }
+
+const HomeIcon = (props) => (
+  <SvgIcon {...props} >
+    {<path
+      d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/>}
+    </SvgIcon>
+  );
 
 function login() {
   api.requestGithub();
@@ -32,13 +40,13 @@ export default class Navbar extends React.Component {
       <div className="containerd-fluid">
         <AppBar showMenuIconButton={false} titleStyle={{flex: 0}}>
           <div className="container">
-            <AppBar
-              title="Jobs."
-              zDepth = {0}
-              showMenuIconButton={false}
-              iconElementRight = {(this.props.token==null)? this.beforeLogin(): this.afterLogin()}
-            /></div>
-        </AppBar>
+        <AppBar
+          title="Jobs"
+          zDepth = {0}
+          showMenuIconButton={false}
+          iconElementRight = {(this.props.token==null)? this.beforeLogin(): this.afterLogin()}
+        /></div>
+      </AppBar>
         {/* <nav className="navbar navbar-light navbar-expand-lg navbar-blue">
           <a className="navbar-brand text-light">Jobs.</a>
           <UserOptions />
@@ -50,11 +58,14 @@ export default class Navbar extends React.Component {
 beforeLogin() {
   return(
     <div>
-      {/*<a href="https://github.com/login/oauth/authorize?scope=user:email&client_id=bdd82a1189d62daed1e5"><IconButton
-        iconClassName="muidocs-icon-custom-github" tooltip="Login With Github"
-        tooltipPosition="bottom-right"
-      /></a>*/}
-      <Link to="/register"><FlatButton {...this.props}  label="Register/Login" labelStyle={{ color: 'white', fontSize: '16px' }} /></Link>
+    {/*<a href="https://github.com/login/oauth/authorize?scope=user:email&client_id=bdd82a1189d62daed1e5"><IconButton
+      iconClassName="muidocs-icon-custom-github" tooltip="Login With Github"
+      tooltipPosition="bottom-right"
+    /></a>*/}
+
+    <Link to="/"><FlatButton {...this.props}  label="Home" labelStyle={{ color: 'white', fontSize: '16px' }}/></Link>
+    <Link to="/register"><FlatButton {...this.props}  label="Register/Login" labelStyle={{ color: 'white', fontSize: '16px' }}/></Link>
+    
     </div>
   )
 }
