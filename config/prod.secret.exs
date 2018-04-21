39bb06de767da0fd7857a5e781765126ab403726
abhishek ahuja <ahuja.a@husky.neu.edu>
2018-04-21 15:36:00 -0400

    added some files

---
+++ config/prod.secret.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you'll likely want to automate and keep away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or yourself later on).
+config :jobPortal, jobPortalWeb.Endpoint,
+  secret_key_base: "S9UEUL+KjeTsGtW79s8TGEx8VaRLaLY+6YN/10XUjQDeUdXnWf1uiALpqSpJqk2m"
+
+# Configure your database
+config :jobPortal, jobPortal.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "jobPortal",
+  password: "someRandomNumber123",
+  database: "jobPortal_prod",
+  pool_size: 15
